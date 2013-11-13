98119602c634a065dbc5e156bc6f74bd91eae966
luxueyan <luxueyan@umeng.com>
2013-11-13 23:57:22 +0800

    mac os compatible

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    nodewebkit: {
+      options: {
+          build_dir: '../', // Where the build version of my node-webkit app is saved
+          mac: true, // We want to build it for mac
+          win: false, // We want to build it for win
+          linux32: false, // We don't need linux32
+          linux64: false // We don't need linux64
+      },
+      src: ['../windowsapp/**/*'] // Your node-wekit app
+    }
+
+  });
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.registerTask('default', [ 'nodewebkit' ]);
+};
